575caf345fea40f9940b72c1a9e8d09c300b4b1a
Honey Patel <honeypatel@Honeys-MacBook-Air.local>
2022-03-09 16:02:11 -0500

    newChanges

--- app/app/src/main/java/com/georgian/farmington/MainActivity.kt
+++ app/app/src/main/java/com/georgian/farmington/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.georgian.farmington
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val agriNewsButtonVar: Button = findViewById (R.id.agrinewsButton)
+        agriNewsButtonVar.setOnClickListener()
+        {
+            val intent = Intent(this, AgriNewsActivity::class.java)
+            startActivity(intent)
+        }
+
+        val weatherButtonVar: Button = findViewById (R.id.weatherButton)
+        weatherButtonVar.setOnClickListener()
+        {
+            val intent = Intent(this, WeatherActivity::class.java)
+            startActivity(intent)
+        }
+        val profile: Button = findViewById (R.id.profilebutton)
+        profile.setOnClickListener()
+        {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
