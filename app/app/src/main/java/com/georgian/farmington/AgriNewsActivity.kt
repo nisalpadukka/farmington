f4291b4747ae2ddd6e1ff2525e8c791853dcfeb4
nisalpadukka <nisalnadeera@gmail.com>
2022-03-28 18:57:25 -0600

    Article Firebase added

--- app/app/src/main/java/com/georgian/farmington/AgriNewsActivity.kt
+++ app/app/src/main/java/com/georgian/farmington/AgriNewsActivity.kt
@@ -1,11 +1,10 @@
 package com.georgian.farmington
 
 import android.os.Bundle
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.georgian.farmington.databinding.ActivityNewsBinding
+import com.google.firebase.firestore.*
 
 class AgriNewsActivity : AppCompatActivity() {
 
--- app/app/src/main/java/com/georgian/farmington/AgriNewsRecyclerViewAdapter.kt
+++ app/app/src/main/java/com/georgian/farmington/AgriNewsRecyclerViewAdapter.kt
@@ -1,30 +1,14 @@
 package com.georgian.farmington
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import okhttp3.*
-import java.io.IOException
 
+class AgriNewsRecyclerViewAdapter(private val articleList: ArrayList<Article>) : RecyclerView.Adapter<AgriNewsRecyclerViewAdapter.ViewHolder>() {
 
-class AgriNewsRecyclerViewAdapter : RecyclerView.Adapter<AgriNewsRecyclerViewAdapter.ViewHolder>() {
-
-    private val client = OkHttpClient()
-
-    private var titles = arrayOf("Agricultural News Heading 1", "Agricultural News Heading 2", "Agricultural News Heading 3",
-        "Agricultural News Heading 4","Agricultural News Heading 5","Agricultural News Heading 6")
-    private var details = arrayOf("Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.")
-    private var images = intArrayOf(R.drawable.farmer_news, R.drawable.farmer_news,
-        R.drawable.farmer_news, R.drawable.farmer_news, R.drawable.farmer_news, R.drawable.farmer_news)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.agri_news_home_card_layout, parent, false)
@@ -32,30 +16,15 @@ class AgriNewsRecyclerViewAdapter : RecyclerView.Adapter<AgriNewsRecyclerViewAda
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.articleTitle.text = titles[position]
-        holder.articleDesc.text = details[position]
-        holder.articleImage.setImageResource(images[position])
+        val article:Article = articleList[position]
+        holder.articleTitle.text = article.title
+        holder.articleDesc.text = article.description
+        //holder.articleImage.setImageResource(articleList[position])
     }
 
     override fun getItemCount(): Int {
-        return titles.size
+        return articleList.size
     }
-
-    fun run(url: String) {
-        val request = Request.Builder()
-            .url(url)
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                Log.i("API", e.toString())
-            }
-            override fun onResponse(call: Call, response: Response) {
-                Log.i("API", response.body()?.string().toString())
-            }
-        })
-    }
-
     inner class ViewHolder(articleView: View): RecyclerView.ViewHolder(articleView){
 
         var articleImage:ImageView
@@ -67,8 +36,5 @@ class AgriNewsRecyclerViewAdapter : RecyclerView.Adapter<AgriNewsRecyclerViewAda
             articleTitle = articleView.findViewById(R.id.article_title)
             articleDesc = articleView.findViewById(R.id.article_detail)
         }
-
-
-
     }
 }
