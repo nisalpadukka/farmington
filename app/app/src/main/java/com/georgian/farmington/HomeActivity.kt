80a61ab4465a5346c55f1f5c5a33f36ed0071a73
honey1510 <98910916+honey1510@users.noreply.github.com>
2022-03-30 12:51:29 -0400

    Getting Profile Img from backend

--- app/app/src/main/java/com/georgian/farmington/HomeActivity.kt
+++ app/app/src/main/java/com/georgian/farmington/HomeActivity.kt
@@ -1,27 +1,60 @@
 package com.georgian.farmington
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import java.io.File
 
 class HomeActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        // Create a storage reference from our app
+        val storageRef = Firebase.storage.reference
 
+// Create a reference with an initial file path and name
+        val pathReference = storageRef.child("image/imgg.png")
 
-        val agriNewsButtonVar: Button = findViewById (R.id.agrinewsButton)
+        val localFile = File.createTempFile("imgg", "png")
+
+         pathReference.getFile(localFile).addOnSuccessListener {
+             // Local temp file has been created
+             val profileImgImageView : ImageButton = findViewById(R.id.profileButton)
+             profileImgImageView.setImageBitmap(BitmapFactory.decodeFile(localFile.absolutePath))
+
+         }.addOnFailureListener {
+             // Handle any errors
+         }
+
+        val agriNewsButtonVar: ImageButton = findViewById (R.id.agrinewsButton)
         agriNewsButtonVar.setOnClickListener()
         {
             val intent = Intent(this, AgriNewsHomeActivity::class.java)
             startActivity(intent)
         }
 
-        val weatherButtonVar: Button = findViewById (R.id.weatherButton)
+        val weatherButtonVar: ImageButton = findViewById (R.id.weatherButton)
         weatherButtonVar.setOnClickListener()
         {
             val intent = Intent(this, WeatherActivity::class.java)
+            startActivity(intent)
+        }
+
+        val profileButtonVar: ImageButton = findViewById (R.id.profileButton)
+        profileButtonVar.setOnClickListener()
+        {
+            val intent = Intent(this, ProfilePageActivity::class.java)
             startActivity(intent)
         }
 
--- app/app/src/main/java/com/georgian/farmington/ProfilePageActivity.kt
+++ app/app/src/main/java/com/georgian/farmington/ProfilePageActivity.kt
@@ -1,11 +1,32 @@
 package com.georgian.farmington
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.io.File
 
 class ProfilePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_page)
+        val storageRef = Firebase.storage.reference
+
+// Create a reference with an initial file path and name
+        val pathReference = storageRef.child("image/imgg.png")
+
+        val localFile = File.createTempFile("imgg", "png")
+
+        pathReference.getFile(localFile).addOnSuccessListener {
+            // Local temp file has been created
+            val profileImgImageView : ImageView = findViewById(R.id.profileImg)
+            profileImgImageView.setImageBitmap(BitmapFactory.decodeFile(localFile.absolutePath))
+
+        }.addOnFailureListener {
+            // Handle any errors
+        }
     }
 }
