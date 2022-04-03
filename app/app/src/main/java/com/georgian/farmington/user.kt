e55530c3e5f69ec2db962da0d3d5bc8fda779e99
Radhika Patel <radhikap0203@gmail.com>
2022-04-03 19:27:44 -0400

    Rebase from develop

---
+++ app/app/src/main/java/com/georgian/farmington/user.kt
@@ -0,0 +1,10 @@
+package com.georgian.farmington
+
+import android.widget.EditText
+
+class user(var fName: EditText,var lName: EditText,var cPwd: EditText,var mobile: EditText) {
+    var fname = fName
+    var lname = lName
+    var cpwd = cPwd
+    var phone = mobile
+}
