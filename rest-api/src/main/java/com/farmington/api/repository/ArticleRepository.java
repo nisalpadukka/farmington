0265e1b3c5ceb3a57979fc962aed53aa7eba9bd7
nisalpadukka <nisalnadeera@gmail.com>
2022-07-27 23:11:56 -0400

    Rest api removed

--- rest-api/src/main/java/com/farmington/api/repository/ArticleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.farmington.api.repository;
-
-import com.farmington.api.entity.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article,Integer> {
-    Article findById(String id);
-}
-
